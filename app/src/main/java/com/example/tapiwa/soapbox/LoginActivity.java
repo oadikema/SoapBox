449cb27177a2fc942916390febb4ae9ea4f4e18f
Tapszvidzwa <zvidzwat@grinnell.edu>
2018-04-21 23:24:25 -0500

    new ui

--- app/src/main/java/com/example/tapiwa/soapbox/LoginActivity.java
+++ app/src/main/java/com/example/tapiwa/soapbox/LoginActivity.java
@@ -29,6 +29,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,11 +41,16 @@ import static android.Manifest.permission.READ_CONTACTS;
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
 
